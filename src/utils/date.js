ef98cdeb9b350a4e4dabd2d863034af759cce5e9
Athena Liu <sdathena@mail.ubc.ca>
2019-01-21 15:10:32 -0800

    Refactor formatDate helper function to use dayjs

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,9 +1,7 @@
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
 export function formatDate(d) {
-  const month = d.slice(5, 7).replace(/^0/, "");
-  const day = d.slice(8).replace(/^0/, "");
-  return `${d.slice(0, 4)}年 ${month}月 ${day}日`;
+  return dayjs(d).format("YYYY年 M月 D 日");
 }
 
-export const getToday = () => dayjs().format('YYYY-MM-DD');
+export const getToday = () => dayjs().format("YYYY-MM-DD");
